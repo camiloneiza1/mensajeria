df172cd57f2d6cc89dfebe13e65ae049a518ae5b
Camilo Neiza <camiloneiza1@gmail.com>
2020-12-15 23:08:54 -0500

    Creacion de rutas para creacion y edicion de mensajeros en base de datos

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Consulta todos los mensajeros de la base de datos
 Route::get('/mensajeros', [MensajeroController::class, 'index'])->name('mensajero.index');
+
+//Mostrar formulario para crear
+Route::get('/mensajeros/create' , [MensajeroController::class, 'create'])->name('mensajero.create');
+
+//Mostrar formulario para editar
+Route::get('/mensajeros/{id}' , [MensajeroController::class, 'edit'])->name('mensajero.edit');
+
+//Crea un mensajero en la base de daros
+Route::post('/mensajeros/store', [MensajeroController::class, 'store'])->name('mensajero.store');
+
+//Edita un mensajero en la base de daros
+Route::put('/mensajeros/update/{id}', [MensajeroController::class, 'update'])->name('mensajero.update');
+
+
+
