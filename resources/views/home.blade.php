0a223d20a64cb8c7820673a1a1a37770d16587b5
camiloneiza1 <camiloneiza1@gmail.com>
2020-12-18 23:05:09 -0500

    implementacion API noticias en Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,86 +1,145 @@
 @extends('layout.app')
 @section('content')
-    <main role="main">
+<main role="main">
+  <div class="container marketing">
 
-
-      <div class="container marketing">
-
-        <!-- Three columns of text below the carousel -->
-        <div class="row">
-          <div class="col-lg-4">
-            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Heading</h2>
-            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
-          </div><!-- /.col-lg-4 -->
-          <div class="col-lg-4">
-            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
-          </div><!-- /.col-lg-4 -->
-        </div><!-- /.row -->
-
-
-        <!-- START THE FEATURETTES -->
-
-        <hr class="featurette-divider">
-
-        <div class="row featurette">
-          <div class="col-md-7">
-            <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-          </div>
-          <div class="col-md-5">
-            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 500px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17673f4091d%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17673f4091d%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22184.890625%22%20y%3D%22261.1%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
-          </div>
+    <!-- Three columns of text below the carousel -->
+    <div class="row">
+      <div class="col-md-3">
+        <div class="form-group">
+          <label for="typeQuery">Tipo Consulta</label>
+          <select id="typeQuery" class="form-control" onchange="typeNews(this.value)">
+            <option value="">Seleccione...</option>
+            <option value="top-headlines">Top Noticias</option>
+            <option value="everything">Todo</option>
+          </select>
         </div>
+      </div>
 
-        <hr class="featurette-divider">
-
-        <div class="row featurette">
-          <div class="col-md-7 order-md-2">
-            <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-          </div>
-          <div class="col-md-5 order-md-1">
-            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17673f4091f%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17673f4091f%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22184.890625%22%20y%3D%22261.1%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="width: 500px; height: 500px;">
-          </div>
+      <div class="col-md-3" id="div_category" style="display: none">
+        <div class="form-group">
+          <label for="category">Categorias</label>
+          <select id="category" class="form-control">
+            <option value="">Seleccione...</option>
+            <option value="technology">Tecnologia</option>
+            <option value="business">Negocios</option>
+            <option value="science">Ciencia</option>
+            <option value="sports">Deportes</option>
+            <option value="health">Salud</option>
+          </select>
         </div>
+      </div>
 
-        <hr class="featurette-divider">
-
-        <div class="row featurette">
-          <div class="col-md-7">
-            <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-          </div>
-          <div class="col-md-5">
-            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17673f40920%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17673f40920%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22184.890625%22%20y%3D%22261.1%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="width: 500px; height: 500px;">
-          </div>
+      <div class="col-md-3" id="div_query" style="display: none">
+        <div class="form-group">
+          <label for="query">Buscar</label>
+          <input type="text" id="query" class="form-control">
         </div>
+      </div>
 
-        <hr class="featurette-divider">
+      <div class="col-md-1">
+        <button type="button" onclick="loadNews()" class="btn btn-primary mt-4">
+          Buscar
+        </button>
+      </div>
 
-        <!-- /END THE FEATURETTES -->
-
-      </div><!-- /.container -->
+    </div><!-- /.row -->
 
 
-      <!-- FOOTER -->
-      <footer class="container">
-        <p class="float-right"><a href="#">Back to top</a></p>
-        <p>© 2017-2018 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
-      </footer>
-    </main>
+    <!-- START THE FEATURETTES -->
 
-  
+    <hr class="featurette-divider">
 
-    <svg xmlns="http://www.w3.org/2000/svg" width="500" height="500" viewBox="0 0 500 500" preserveAspectRatio="none" style="display: none; visibility: hidden; position: absolute; top: -100%; left: -100%;"><defs><style type="text/css"></style></defs><text x="0" y="25" style="font-weight:bold;font-size:25pt;font-family:Arial, Helvetica, Open Sans, sans-serif">500x500</text></svg></body>
+    <div class="row" id="news"></div>
+
+
+    <!-- /END THE FEATURETTES -->
+
+  </div><!-- /.container -->
+
+
+  <!-- FOOTER -->
+  <footer class="container">
+    <p class="float-right"><a href="#">Back to top</a></p>
+    <p>© 2017-2018 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+  </footer>
+</main>
+</body>
+@endsection
+
+@section('scripts')
+<script>
+  $(document).ready(function(){
+    loadNews()
+  });
+
+  function loadNews() {
+    const url = "https://newsapi.org/v2";
+    const apiKey = "de8e05adc57846ceac44b904a1fcc6ba";
+
+    let typeQuery = $('#typeQuery').val();
+    let category = $('#category').val();
+    let query = $('#query').val();
+
+    
+    if (typeQuery == "") {
+      typeQuery = "top-headlines";
+    }
+    
+    let datos = {
+      apiKey: apiKey,
+      country: "co",
+    }
+
+    if (typeQuery === 'everything') {
+      datos = {
+        apiKey: apiKey,
+        q:query,
+        language: "es"
+      }
+    }else{
+      datos = {
+        apiKey: apiKey,
+        country: "co",
+        category: category,
+      }
+    }
+    console.log(datos);
+    $.ajax({
+      url: `${url}/${typeQuery}`,
+      data:datos,
+      type: "GET",
+      dataType: 'json',
+      success: function(res) {
+        let noticias = '';
+        for (let i = 0; i < res.articles.length; i++) {
+          const noticia = res.articles[i];
+          noticias = noticias+`<div class="row featurette mb-3">
+                      <div class="col-md-7">
+                        <h2 class="featurette-heading">${noticia.title}</h2>
+                        <p class="lead">${noticia.description}. <a href="${noticia.url}" target="_blank">Ver mas</a></p>
+                      </div>
+                      <div class="col-md-5">
+                        <img class="featurette-image img-fluid mx-auto"  alt="noticia" src="${noticia.urlToImage}" data-holder-rendered="true">
+                        <p>${noticia.publishedAt}</p>
+                      </div>
+                    </div>
+                    <hr class="featurette-divider">`
+        }
+        
+        $('#news').html(noticias)
+      }
+    });
+  }
+
+  function typeNews(Type) {
+    if (Type === 'everything') {
+      $('#div_category').hide();
+      $('#div_query').show();
+    }else{
+      $('#div_query').hide();
+      $('#div_category').show();
+    }
+  }
+</script>
 @endsection
