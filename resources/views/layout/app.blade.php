3d305112cf3d31c346efff38d4aa474b007cc72a
Camilo Neiza <camiloneiza1@gmail.com>
2020-12-22 23:17:05 -0500

    inclucion de datetimepiker

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    {!! Html::style('css/bootstrap-datetimepicker.min.css') !!}
     <title>Mensajeria</title>
 </head>
 <body>
@@ -24,7 +25,7 @@
             <a class="nav-link" href="/clientes">Clientes</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Ordenes</a>
+            <a class="nav-link" href="/ordenes">Ordenes</a>
           </li>
         </ul>
       </div>
@@ -40,6 +41,8 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/fa34ae4598.js" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+{!! Html::script('js/bootstrap-datetimepicker.min.js') !!}
 
 @yield('scripts')
 </html>
